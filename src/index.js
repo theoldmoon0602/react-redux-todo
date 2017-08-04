eb156e8850fd22f1ab41da128d56c51e24b2c0da
theoldmoon0602 <theoldmoon0602@gmail.com>
2017-08-04 14:50:59 +0900

    Redux element creating and Testing
    ActionCreator, Reducer, Store

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
-alert("HELLO")
+import expect from 'expect'
+import { createStore } from 'redux'
+
+// ActionCreator 新しいTodoを追加するAction
+const AddNewTodo = (content) => {
+    return {
+	type: 'ADD_NEW_TODO',
+	content
+    }
+}
+
+// Reducer. actionから新しいstateを返す
+const reducer = (state = {todos: []}, action) => {
+    switch (action.type) {
+	case 'ADD_NEW_TODO':
+	    return {
+		todos: [
+		    ...state.todos,
+		    {
+			state: 'TODO',
+			content: action.content
+		    }
+		]
+	    }
+	default:
+	    return state
+    }
+}
+
+// store. 唯一の状態を持つ
+const store = createStore(reducer)
+
+expect((() => {
+    store.dispatch(AddNewTodo('todo1'))
+    return store.getState()
+})()).toEqual({
+    todos: [
+	{
+	    state: 'TODO',
+	    content: 'todo1'
+	}
+    ]
+})
+
+alert('ALL TEST PASSED')
